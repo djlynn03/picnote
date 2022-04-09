58550d212ab02fd702190a36e88987e8580796a6
djlynn03 <djlynn03@gmail.com>
2022-04-09 12:25:49 -0500

    utils

--- imageparse/utils.py
+++ imageparse/utils.py
@@ -1,6 +1,14 @@
 from PIL import Image
+import pillow_heif
 
 def image_intake(img):
+    
+    if img.name.endswith('.heif') or img.name.endswith('.heic') or img.name.endswith('.HEIF') or img.name.endswith('.HEIC'):
+        img = pillow_heif.open(img)
+        img = Image.frombytes(img.mode, img.size, img.data, "raw")
+        img = img.convert('L')
+        return img
+    
     img = Image.open(img)
     img = img.convert('L')
     return img
