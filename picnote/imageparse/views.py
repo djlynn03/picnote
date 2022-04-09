130b5c808343660ddccdc4ba78345013e73a28a8
djlynn03 <djlynn03@gmail.com>
2022-04-08 21:43:14 -0500

    urls

--- picnote/imageparse/views.py
+++ picnote/imageparse/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views import generic
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'imageparse/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
