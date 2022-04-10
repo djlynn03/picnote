f648daeeba082826f60b1be2cd6bae6003744393
djlynn03 <djlynn03@gmail.com>
2022-04-09 23:19:39 -0500

    fixed download

--- imageparse/views.py
+++ imageparse/views.py
@@ -1,3 +1,4 @@
+from encodings import utf_8
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views import generic
@@ -7,11 +8,8 @@ from PIL import Image
 from .imageparse import transcribeAll, image2file
 import os
 import base64
-from docx2pdf import convert
 from .utils import image_intake
-import pythoncom
-
-
+import sys
 class IndexView(generic.ListView):
     template_name = 'imageparse/index.html'
     context_object_name = 'latest_question_list'
@@ -28,20 +26,15 @@ class IndexView(generic.ListView):
             return render(request, self.template_name, {'error': 'Invalid file type'})
 
         from io import BytesIO
-        pythoncom.CoInitialize()
+
 
         final_image[0].save("test.jpg")
         final_image = transcribeAll("test.jpg")
-        # os.remove("test.jpg")
 
         buffered = BytesIO()
         final_image.save(buffered)
-        convert(os.path.join(settings.BASE_DIR, "test_document.docx"))
-        
-        pdf_file = base64.b64encode(
-            open(os.path.join(settings.BASE_DIR, "test_document.pdf"), "rb").read())
-        
-        return render(request, self.template_name, {'output': "data:application/pdf;base64," + pdf_file.decode('utf-8') + "#toolbar=0&navpanes=0&scrollbar=0", "docx": "data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64," + base64.b64encode(open(os.path.join(settings.BASE_DIR, "test_document.docx"), "rb").read()).decode('utf-8')})
+
+        return render(request, self.template_name, {"docx": "data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64," + base64.b64encode(open(os.path.join(settings.BASE_DIR, "test_document.docx"), "rb").read()).decode('utf-8')})
 
 
 def upload(request, *args, **kwargs):
